7a68f47a23afb4121f9923176034a66d03b86e13
paul0908 <paul.david.hiller@mni.thm.de>
2024-02-16 21:22:45 +0100

    simple and finished

--- gpio-fan-control/src/main.rs
+++ gpio-fan-control/src/main.rs
@@ -1,12 +1,43 @@
-use std::fs;
+use std::{fs, io, thread};
+use std::time::Duration;
+use gpio::GpioOut;
+use gpio::sysfs::SysFsGpioOutput;
+use log::{error, info};
+use simple_logger::SimpleLogger;
 
+const TEMP_FILE_PATH: &str = "/sys/class/thermal/thermal_zone0/temp";
+const GPIO_PIN: u16 = 73;
+const CHECK_INTERVAL: Duration = Duration::from_secs(5);
+const TEMP_THRESHOLD: f32 = 50.0;
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    SimpleLogger::new().init().unwrap();
+    let mut gpio_fan_pin = SysFsGpioOutput::open(GPIO_PIN).unwrap();
+    info!("fan control initialized");
+    loop {
+        match read_temp(TEMP_FILE_PATH) {
+            Ok(temp) if temp >= TEMP_THRESHOLD => {
+                if let Err(e) = gpio_fan_pin.set_high() {
+                    error!("Failed to set GPIO pin high: {} at temperature: {}", e, temp);
+                }
+            }
+            Ok(temp) => {
+                if let Err(e) = gpio_fan_pin.set_low() {
+                    error!("Failed to set GPIO pin low: {} at temperature: {}", e, temp);
+                }
+            }
+            Err(e) => {
+                error!("Failed to read temperature: {}", e);
+                thread::sleep(CHECK_INTERVAL);
+                continue;
+            }
+        }
+        thread::sleep(CHECK_INTERVAL);
+    }
+}
 
-fn main() {
-    const TEMP_FILE_NAME: &str = "/sys/class/thermal/thermal_zone0/temp";
-    const GPIO_PIN_FAN: i8 = 2;
-
-    let content = fs::read_to_string(TEMP_FILE_NAME)
-        .expect("Unable to read temp from file");
-
-    println!("File contents: {}, {}", content, GPIO_PIN_FAN)
+fn read_temp(file_path: &str) -> Result<f32, io::Error> {
+    let content = fs::read_to_string(file_path)?;
+    content.trim().parse::<f32>()
+        .map(|temp_milli| temp_milli / 1000.0)
+        .map_err(|_| io::Error::new(io::ErrorKind::InvalidData, "Could not parse the temperature"))
 }
